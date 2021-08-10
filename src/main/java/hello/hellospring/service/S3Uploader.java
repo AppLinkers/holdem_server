b0c9ecc2da89e408f418ff6f042e5aabd76067cc
yuseogi0218 <yuseogi0218@gmail.com>
2021-08-10 23:23:53 +0900

    test

--- src/main/java/hello/hellospring/service/S3Uploader.java
+++ src/main/java/hello/hellospring/service/S3Uploader.java
@@ -26,6 +26,8 @@ public class S3Uploader {
     public String bucket; // S3 버킷 이름
 
     public String upload(MultipartFile multipartFile, String dirName) throws IOException {
+        log.info("start_log");
+        System.out.println("start");
         File uploadFile = convert(multipartFile) // 파일 변환할 수 없으면 에러
             .orElseThrow(() -> new IllegalArgumentException("error: MultipartFile -> File Conver Fail"));
 
