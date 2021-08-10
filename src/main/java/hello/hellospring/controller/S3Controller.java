00c78acb1435ad7520883e665b8d091a81f836be
yuseogi0218 <yuseogi0218@gmail.com>
2021-08-11 08:39:21 +0900

    test

--- src/main/java/hello/hellospring/controller/S3Controller.java
+++ src/main/java/hello/hellospring/controller/S3Controller.java
@@ -1,7 +1,7 @@
 package hello.hellospring.controller;
 
 import hello.hellospring.service.S3Uploader;
-import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -11,11 +11,15 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 
-@RequiredArgsConstructor
 @Controller
 public class S3Controller {
 
     private final S3Uploader s3Uploader;
+
+    @Autowired
+    public S3Controller(S3Uploader s3Uploader) {
+        this.s3Uploader = s3Uploader;
+    }
 
     @GetMapping("/image")
     public String imgTest() {
