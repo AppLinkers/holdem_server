5149d8b32a8011bf07ddc772ce9fc41096ef5ca3
yuseogi0218 <yuseogi0218@gmail.com>
2021-08-10 23:13:05 +0900

    test

--- src/main/java/hello/hellospring/service/S3Uploader.java
+++ src/main/java/hello/hellospring/service/S3Uploader.java
@@ -63,8 +63,8 @@ public class S3Uploader {
 
     //로컬에 파일 업로드 하기
     private Optional<File> convert(MultipartFile file) throws IOException {
-        File convertFile = new File(file.getOriginalFilename());
-        // System.getProperty("user.dir") + "/" +
+        File convertFile = new File(System.getProperty("user.dir") + "/" + file.getOriginalFilename());
+
         // 바로 위에서 지정한 경로에 File 이 생성됨 (경로가 잘못되었다면 생성 불가능) 해당 파일은 removeNewFile 을 통해 로컬에서 지워짐
         if (convertFile.createNewFile()) {
             try (FileOutputStream fos = new FileOutputStream(convertFile)) { // FileOutputStream 데이터를 파일에 바이트 스트림으로 저장하기 위함
