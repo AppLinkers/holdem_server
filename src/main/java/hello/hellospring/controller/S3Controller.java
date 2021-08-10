0a95164fa50d43556149adba69f425fb68681ee5
yuseogi0218 <yuseogi0218@gmail.com>
2021-08-11 08:26:02 +0900

    test

--- src/main/java/hello/hellospring/controller/S3Controller.java
+++ src/main/java/hello/hellospring/controller/S3Controller.java
@@ -2,20 +2,28 @@ package hello.hellospring.controller;
 
 import hello.hellospring.service.S3Uploader;
 import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 
 @RequiredArgsConstructor
-@RestController
+@Controller
 public class S3Controller {
 
     private final S3Uploader s3Uploader;
 
+    @GetMapping("/image")
+    public String imgTest() {
+        return "imgTest";
+    }
+
     @PostMapping("/images")
+    @ResponseBody
     public String upload(@RequestParam("images")MultipartFile multipartFile) throws IOException {
         s3Uploader.upload(multipartFile, "static");
         return "test";
