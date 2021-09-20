d3ebec5dbb990156734d30d0bc84762a88f6fbe5
yuseogi0218 <yuseogi0218@gmail.com>
2021-09-20 18:40:41 +0900

    signup_error

--- src/main/java/hello/hellospring/service/MemberService.java
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -29,7 +29,7 @@ public class MemberService {
     }
 
     private void validateDuplicateMember(Member member) {
-        memberRepository.findByName(member.getUser_id())
+        memberRepository.findById(member.getUser_id())
                 .ifPresent(m -> {
                     throw new IllegalStateException("이미 존재하는 회원입니다.");
                 } );
